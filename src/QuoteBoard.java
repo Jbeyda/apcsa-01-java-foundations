7cca51f74f2549243efdb6a74e49f9756d242106
Jbeyda <87097503+Jbeyda@users.noreply.github.com>
2026-09-22 20:25:38 +0000

    quote

--- src/QuoteBoard.java
+++ src/QuoteBoard.java
@@ -18,7 +18,7 @@
  */
 public class QuoteBoard {
     public static void main(String[] args) {
-
+System.out.println("\"You miss one hundred perent of the shots you dont take- Wayne Gretzky.\"");
         // Your code here
 
     }
